4f57ff82ac75026c1e9baa19c81f49fcb03e2fb8
Iana Pylaeva <MPiY@yandex.ru>
2022-07-28 16:21:00 +0300

    D

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Profile from "../Profile/Profile";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <Routes>
+        <Route exact path="/">
+          <Header />
+          <Main />
+          <Footer />
+        </Route>
+        <Route exact path="/movies">
+          <Header />
+          <Movies />
+          <Footer />
+        </Route>
+        <Route exact path="/saved-movies">
+          <Header />
+          <SavedMovies />
+          <Footer />
+        </Route>
+        <Route exact path="/profile">
+          <Header />
+          <Profile />
+        </Route>
+        <Route exact path="/signin">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <Register />
+        <Route exact path="*">
+          <PageNotFound />
+        </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
