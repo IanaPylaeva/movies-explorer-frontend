e6f4cea32b84ee25d8a714006b7316ea90835dc3
Iana Pylaeva <MPiY@yandex.ru>
2022-07-28 19:05:28 +0300

    D

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Profile() {
+  function handleSubmit(evt) {
+    evt.preventDefault();// Запрещаем браузеру переходить по адресу формы - не перезагружается
+  }
+
+  return (
+    <section className="profile">
+      <form className="profile__form" onSubmit={handleSubmit} name="profile-form">
+        <h2 className="profile__title">Привет, 'Виталий'!</h2>
+        <div className="profile__inputs">
+          <div className="profile__input-container">          
+            <p className="profile__text">Имя</p>
+            <input className="profile__input" value="Виталий" placeholder="Имя" type="text" name="name-user" minLength="1" maxLength="50" required />
+          </div>
+          <div className="profile__input-container">          
+            <p className="profile__text">E-mail</p>
+            <input className="profile__input" value="pochta@yandex.ru" placeholder="E-mail" type="email" name="name-user" minLength="1" maxLength="50"  required />
+          </div>
+        </div>
+      </form>
+      <button className="profile__button-edit" type="submit">Редактировать</button>
+      <button className="profile__button-sign-out" type="button">Выйти из аккаунта</button>
+    </section>
+  )
+}
+
+export default Profile;
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -3,7 +3,6 @@ import './SavedMovies.css';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
-
 function SavedMovies() {
   return (
     <div className="saved-movies">
