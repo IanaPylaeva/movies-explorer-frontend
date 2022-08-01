e27087a0a2dec87636362d901bf131cbefa95625
Iana Pylaeva <MPiY@yandex.ru>
2022-08-01 13:46:41 +0300

    D

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,19 @@ import React from "react";
 import './Footer.css';
 
 function Footer() {
-
+  return (
+    <footer className="footer">
+      <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+      <div className="footer__container">
+        <div className="footer__date">&copy; 2022</div>
+        <ul className="footer__list-links">
+          <li className="footer__link">Яндекс.Практикум</li>
+          <li className="footer__link">Github</li>
+          <li className="footer__link">Facebook</li>
+        </ul>
+      </div>
+    </footer> 
+  )
 }
 
 export default Footer;
