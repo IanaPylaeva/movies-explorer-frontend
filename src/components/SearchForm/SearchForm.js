b4150cd2bcc62ffe8a7bfbd8f040aca032cdad0a
Iana Pylaeva <MPiY@yandex.ru>
2022-08-02 16:07:33 +0300

    D

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,18 +1,20 @@
 import React from "react";
 import './SearchForm.css';
+import loupe from '../../images/loupe.svg';
 
 function SearchForm() {
   return (
     <section className="search-form">
       <div className="search-form__container">
         <div className="search-form__film-container">
-          <button className="search-form__button-loupe" type="button"></button>
+          <img src={loupe} className="search-form__loupe" alt="значок лупа" />
           <input className="search-form__input-film" placeholder="Фильм" type="text" required />
+          <button className="search-form__button-find" type="submit"></button>
         </div>
         <div className="search-form__short-film-container">
-
+          <button className="search-form__button-switch"></button>
+          <p className="search-form__text">Короткометражки</p>
         </div>
-
       </div>
     </section>
   )
