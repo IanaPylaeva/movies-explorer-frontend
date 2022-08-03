ca0f2190a99a5e06f513490636701b0af667d4da
Iana Pylaeva <MPiY@yandex.ru>
2022-08-03 16:09:08 +0300

    D

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
       <div className="footer__container">
         <div className="footer__date">&copy; 2022</div>
         <ul className="footer__list-links">
-          <li className="footer__link">Яндекс.Практикум</li>
-          <li className="footer__link">Github</li>
-          <li className="footer__link">Facebook</li>
+          <li className="footer__link"><a className="footer__link-name" href="https://practicum.yandex.ru/" target="_blank" rel="noreferrer" >Яндекс.Практикум</a></li>
+          <li className="footer__link"><a className="footer__link-name" href="https://github.com/" target="_blank" rel="noreferrer" >Github</a></li>
+          <li className="footer__link"><a className="footer__link-name" href="https://www.facebook.com/" target="_blank" rel="noreferrer" >Facebook</a></li>
         </ul>
       </div>
     </footer> 
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './Welcome.css';
 import logo from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
 
 function Welcome(props) {
   return (
     <div className="welcome">
-      <img src={logo} alt="логотип" className="welcome__logo" />
+      <Link to="/" className="welcome__logo-link">
+        <img src={logo} alt="логотип" className="welcome__logo" />
+      </Link>
       <h2 className="welcome__title">{props.title}</h2>
     </div>    
   );
