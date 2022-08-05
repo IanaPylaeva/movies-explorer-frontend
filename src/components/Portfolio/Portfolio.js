51fcfaf5a4815404faee56f015d7c7acc84d93db
Iana Pylaeva <MPiY@yandex.ru>
2022-08-05 08:14:45 +0300

    D

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './Portfolio.css';
-import arrow from "../../images/strelka-open.svg";
 
 function Portfolio() {
   return (
@@ -9,15 +8,15 @@ function Portfolio() {
       <ul className="portfolio__list">
         <li className="portfolio__list-item">
           <p className="portfolio__text">Статичный сайт</p>
-          <a href="https://#" target="_blank" rel="noreferrer" className="portfolio__arrow"><img src={arrow} alt="стрелка-ссылка" /></a>
+          <a href="https://#" target="_blank" rel="noreferrer" className="portfolio__arrow">&uarr;</a>
         </li>
         <li className="portfolio__list-item">
           <p className="portfolio__text">Адаптивный сайт</p>
-          <a href="https://#" target="_blank" rel="noreferrer" className="portfolio__arrow"><img src={arrow} alt="стрелка-ссылка" /></a>
+          <a href="https://#" target="_blank" rel="noreferrer" className="portfolio__arrow">&uarr;</a>
         </li>
         <li className="portfolio__list-item">
           <p className="portfolio__text">Одностраничное приложение</p>
-          <a href="https://#" target="_blank" rel="noreferrer" className="portfolio__arrow"><img src={arrow} alt="стрелка-ссылка" /></a>
+          <a href="https://#" target="_blank" rel="noreferrer" className="portfolio__arrow">&uarr;</a>
         </li>
       </ul>
     </section>
