4f82f8fe1fdd559b493ccf4555233de106882685
Iana Pylaeva <MPiY@yandex.ru>
2022-08-05 10:46:10 +0300

    D

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -13,21 +13,19 @@ function Navigation() {
     <nav className="navigation">
       <button className="navigation__button-menu" type="button" onClick={handleToggleMenu}></button>
       <div className={`navigation__container ${showItems ? 'navigation__container_active' : ''}`}>
-        <div className="navigation__sidebar">
-          <button className="navigation__button-close" type="button" onClick={handleToggleMenu}></button>
-          <ul className="navigation__list">
-            <li className="navigation__list-item">
-              <Link to="/" className="navigation__link">Главная</Link>
-            </li>
-            <li className="navigation__list-item">
-              <NavLink to="/movies" className="navigation__link">Фильмы</NavLink>
-            </li>
-            <li className="navigation__list-item">
-              <NavLink to="/saved-movies" className="navigation__link">Сохранённые фильмы</NavLink>
-            </li>
-          </ul>
-          <NavLink to="/profile" className="navigation__link-button">Аккаунт</NavLink>
-        </div>
+        <button className="navigation__button-close" type="button" onClick={handleToggleMenu}></button>
+        <ul className="navigation__list">
+          <li className="navigation__list-item">
+            <Link to="/" className="navigation__link">Главная</Link>
+          </li>
+          <li className="navigation__list-item">
+            <NavLink to="/movies" className="navigation__link">Фильмы</NavLink>
+          </li>
+          <li className="navigation__list-item">
+            <NavLink to="/saved-movies" className="navigation__link">Сохранённые фильмы</NavLink>
+          </li>
+        </ul>
+        <NavLink to="/profile" className="navigation__link-button">Аккаунт</NavLink>
       </div>
     </nav>
   )
