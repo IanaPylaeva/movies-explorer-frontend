7912d0c446b27f7e9ea6d46718098ead71d8e17a
Iana Pylaeva <MPiY@yandex.ru>
2022-08-08 19:04:36 +0300

    D

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,13 +6,15 @@ import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 function SearchForm() {
   return (
     <section className="search-form">
-      <div className="search-form__container">
-        <div className="search-form__film-container">
-          <img src={loupe} className="search-form__loupe" alt="значок лупа" />
-          <input className="search-form__input-film" placeholder="Фильм" type="text" required />
-          <button className="search-form__button-find" type="submit"></button>
+      <div className="search-form__border">
+        <div className="search-form__container">
+          <div className="search-form__film-container">
+            <img src={loupe} className="search-form__loupe" alt="значок лупа" />
+            <input className="search-form__input-film" placeholder="Фильм" type="text" required />
+            <button className="search-form__button-find" type="submit"></button>
+          </div>
+          <FilterCheckbox />
         </div>
-        <FilterCheckbox />
       </div>
     </section>
   )
