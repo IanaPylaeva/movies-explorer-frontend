50a31c0ddcca7ca92f84cb4ad6a8a4774e96093c
Iana Pylaeva <MPiY@yandex.ru>
2022-08-11 15:02:41 +0300

    D

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,17 +6,17 @@ function Portfolio() {
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
-        <li className="portfolio__list-item">
-          <p className="portfolio__text">Статичный сайт</p>
-          <a href="https://ianapylaeva.github.io/how-to-learn/" target="_blank" rel="noreferrer" className="portfolio__arrow">&uarr;</a>
+        <li className="portfolio__list-item">          
+          <a href="https://ianapylaeva.github.io/how-to-learn/" target="_blank" rel="noreferrer" className="portfolio__link">Статичный сайт</a>
+          <a href="https://ianapylaeva.github.io/how-to-learn/" target="_blank" rel="noreferrer" className="portfolio__link"><p className="portfolio__arrow">&uarr;</p></a>
         </li>
         <li className="portfolio__list-item">
-          <p className="portfolio__text">Адаптивный сайт</p>
-          <a href="https://ianapylaeva.github.io/russian-travel/" target="_blank" rel="noreferrer" className="portfolio__arrow">&uarr;</a>
+          <a href="https://ianapylaeva.github.io/russian-travel/" target="_blank" rel="noreferrer" className="portfolio__link">Адаптивный сайт</a>
+          <a href="https://ianapylaeva.github.io/how-to-learn/" target="_blank" rel="noreferrer" className="portfolio__link"><p className="portfolio__arrow">&uarr;</p></a>
         </li>
         <li className="portfolio__list-item">
-          <p className="portfolio__text">Одностраничное приложение</p>
-          <a href="https://domain.ianapylaeva.nomoredomains.xyz" target="_blank" rel="noreferrer" className="portfolio__arrow">&uarr;</a>
+          <a href="https://domain.ianapylaeva.nomoredomains.xyz" target="_blank" rel="noreferrer" className="portfolio__link">Одностраничное приложение</a>
+          <a href="https://ianapylaeva.github.io/how-to-learn/" target="_blank" rel="noreferrer" className="portfolio__link"><p className="portfolio__arrow">&uarr;</p></a>
         </li>
       </ul>
     </section>
