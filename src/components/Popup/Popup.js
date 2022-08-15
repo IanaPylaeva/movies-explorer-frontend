4680c260c0aff376964f05b08cbed01f3c207254
Iana Pylaeva <MPiY@yandex.ru>
2022-08-15 19:51:56 +0300

    D

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -2,7 +2,7 @@ import './Popup.css';
 
 function Popup({ text, isOpen, onClose }) {
   return(
-    <section className={`popup ${isOpen ? 'popup_opened' : ''}`} >
+    <section className={`popup ${isOpen ? 'popup_opened' : ''}`} onClick={onClose}>
       <div className="popup__container">
         <p className="popup__text">{text}</p>
         <button className="popup__close" type="button" onClick={onClose} />
